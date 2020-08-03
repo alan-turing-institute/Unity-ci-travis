732647ae0b1aa7a97be158be7bbf1cd0d5135c68
misspawty <flora.roumpani.11@ucl.ac.uk>
2020-08-03 17:09:10 +0100

    built files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Unity-ci-travis
- Continious integration example with travis and unity3D
--- Scripts/install-unity.sh
+++
@@ -1,1 +0,0 @@
-  #! /bin/sh# See https://unity3d.com/get-unity/download/archive# to get download URLsUNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/46dda1414e51/MacEditorInstaller/Unity-2017.2.0f3.pkg"UNITY_VERSION=""
