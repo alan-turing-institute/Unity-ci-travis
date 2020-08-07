bd56055686adebc752febcca4d781770b3e38a7d
misspawty <flora.roumpani.11@ucl.ac.uk>
2020-08-07 16:09:23 +0100

    Update runtests.sh

--- Scripts/runtests.sh
+++ Scripts/runtests.sh
@@ -1,5 +1,6 @@
 
 echo about to run tests
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -runTests -batchmode -projectPath .
+#/Users/travis/build/alan-turing-institute/Unity-ci-travis/
 
 echo run tests hopefully
