556577621cba64ba42fc3f2d7291e51e5e8943ed
nbarlowATI <nbarlow@turing.ac.uk>
2020-08-17 11:39:15 +0100

    run tests in play mode and test output xml

--- Scripts/runtests.sh
+++ Scripts/runtests.sh
@@ -1,5 +1,12 @@
 
 echo about to run tests
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -runTests -batchmode -projectPath /Users/travis/build/alan-turing-institute/Unity-ci-travis/
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -runTests -batchmode -testPlatform playmode -projectPath /Users/travis/build/alan-turing-institute/Unity-ci-travis/
 
-echo run tests hopefully
+echo run tests hopefully - now check output xml
+echo `ls *.xml`
+output=`grep "One or more child tests had errors" *.xml`
+outputlen=${#output}
+
+if [ $outputlen -gt 0 ]; then
+    return -1
+fi
