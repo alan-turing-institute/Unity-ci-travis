ab744d7a29f8b1d092614bfbb69c9439424d09d6
nbarlowATI <nbarlow@turing.ac.uk>
2020-08-17 15:22:02 +0100

    use env var for unity project location

--- Scripts/runtests.sh
+++ Scripts/runtests.sh
@@ -1,12 +1,16 @@
 
 echo about to run tests
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -runTests -batchmode -testPlatform playmode -projectPath /Users/travis/build/alan-turing-institute/Unity-ci-travis/
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -runTests -batchmode -testPlatform playmode -projectPath $UNITY_PROJECT_DIR
 
 echo run tests hopefully - now check output xml
-echo `ls *.xml`
-output=`grep "One or more child tests had errors" *.xml`
+echo `ls ${UNITY_PROJECT_DIR}/*.xml`
+output=`grep "One or more child tests had errors" ${UNITY_PROJECT_DIR}/*.xml`
 outputlen=${#output}
 
 if [ $outputlen -gt 0 ]; then
-    return -1
+    retcode=44
+else
+    retcode=0
 fi
+
+exit $retcode
