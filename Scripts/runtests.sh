141622f769ef0caeba489a53700c558ca4a6dab4
nbarlowATI <nbarlow@turing.ac.uk>
2020-09-07 17:43:56 +0100

    try to get runtests to write xml file somewhere

--- Scripts/runtests.sh
+++ Scripts/runtests.sh
@@ -1,6 +1,6 @@
 
 echo about to run tests
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -runTests -batchmode -testPlatform playmode -projectPath $UNITY_PROJECT_DIR
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -runTests -batchmode -testPlatform playmode -projectPath $UNITY_PROJECT_DIR -testResults ${UNITY_PROJECT_DIR}/results.xml
 
 echo run tests hopefully - now check output xml
 echo `ls ${UNITY_PROJECT_DIR}/*.xml`
@@ -8,7 +8,7 @@ output=`grep "One or more child tests had errors" ${UNITY_PROJECT_DIR}/*.xml`
 outputlen=${#output}
 
 if [ $outputlen -gt 0 ]; then
-    retcode=44
+    retcode=-1
 else
     retcode=0
 fi
